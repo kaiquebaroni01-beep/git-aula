e83879dd02ba4dd3b6d267ffbe28e5ecf0f3f1fe
kaiky <kaiquebaroni@gmail.com>
2026-03-26 10:04:49 -0300

    aula 26/03

---
+++ Untitled-1.py
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <!-- 02/03/26-s3a2-introdução ao css
+        objetivo: conhecer os conceitos basicos do css -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Klicya</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="so-sim">
+    <h1>Só sim!!</h1>
+    </div>
+
+    <div class="menu">
+    <ul>
+        <div class="content-box"></div><li><a href="#">bbb</a></li></div> <!---Item da lista--->
+        <div class="content-box"></div><li><a href="#">diario</a></li>
+        <div class="content-box"></div><li><a href="#">fim</a></li>
+        
+
+
+        </div>
+    </ul>
+    </div>
+
+    <a style="background-color: rgb(193, 160, 251);">hi girls</a> <br>
+    Um diario para garotas!</p>
+    <p>Se um dia eu achei que estaria onde estou. <em>Eu menti...</em> Nunca pensei que faria o que fiz e o que deixei de fazer. A vida muda com um unico bater de asas da borboleta, e foi isso que fez minha vida virar de cabeça pra baixo, ou melhor... <i>Pra cima.</i> <br>
+    <br>
+    Foi quando conheci a LDS(lei da suposição), essa lei mudou minha maneira de pensar </p>
+
+    <img src="Ana paula.png" width="300" alt="ana-paula-renault">
+    <button>clique aqui</button>
+    
+    <form>
+        <div class="nome">
+        <label>Nome:</label><br>
+        </div>
+        <input type="text"><br><br>
+
+        <label>Data de Nascimento</label>
+        <input type="date"><br><br>
+
+        <label>Email</label>
+        <input type="email"><br><br>
+
+        <button type="submit">Enviar</button>
+
+        </form>
+
+</body>
+</html>
